56529b9137d4249c30346f46e1eb0435ae1f7b86
Katte <37640938+TacocaTYT@users.noreply.github.com>
2023-03-29 15:17:31 -0700

    worked with implementing the web backend JS for handling the database

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
-const sqlite3 = require('sqlite3').verbose();
-
+import sqlite3 from 'sqlite3';
 const rpb = document.getElementById("recentPostButton");
+const db = new sqlite3.Database('./posts.db', (err) => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log('Connected to the database.');
+});
 
-const postTitle = "404";
-let db = new sqlite3.Database('./posts.db');
-
-let sql = 'SELECT ID, TITLE FROM POSTS ORDER BY ID DESC LIMIT 1';
+const sql = 'SELECT ID, TITLE FROM POSTS ORDER BY ID DESC LIMIT 1';
 
 db.get(sql, (err, row) => {
-    if (err) {
-      return console.error(err.message);
-    }
-    if (row) {
-      const postTitle = row.TITLE;
-      const postURL = "404";
-      rpb.textContent = postTitle;
-      rpb.href = postURL;
-    } else {
-      console.log('No posts found in the database');
-    }
-  });
+  if (err) {
+    return console.error(err.message);
+  }
+  if (row) {
+    const postTitle = row.TITLE;
+    const postURL = `/posts/${row.ID}`;
+    rpb.textContent = postTitle;
+    rpb.href = postURL;
+  } else {
+    console.log('No posts found in the database');
+    rpb.postTitle = "404"
+    rpb.href = "404"
+  }
+  db.close();
+});
